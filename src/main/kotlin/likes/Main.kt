612abd6cb0f290a33b4ff5f1fb2d329469ff58c6
Ruslan <karuslan@inbox.ru>
2022-05-20 18:14:32 +0300

    Версия 3.0 с учетом 11 и 111 и т.д. лайков

--- src/main/kotlin/likes/Main.kt
+++ src/main/kotlin/likes/Main.kt
@@ -1,6 +1,6 @@
 package likes
 
 fun main() {
-    var likes: UInt = 11u
-    if (likes % 10u == 1u && likes != 11u) println("Понравилось $likes человеку") else println("Понравилось $likes людям")
+    var likes: UInt = 1211u
+    if (likes % 10u == 1u && likes % 100u != 11u) println("Понравилось $likes человеку") else println("Понравилось $likes людям")
 }
